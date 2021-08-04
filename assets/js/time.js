9b0969a00afedb169e53443441a3bc2c7dac05cb
CertifiedRascal <50462386+CertifiedRascal@users.noreply.github.com>
2021-08-04 13:47:46 -0500

    Update time.js

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,19 +1,29 @@
 window.onload = displayClock();
 function displayClock() {
   const monthNames = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
+    '１月',
+    '２月',
+    '３月',
+    '４月',
+    '５月',
+    '６月',
+    '７月',
+    '８月',
+    '９月',
+    '１０月',
+    '１１月',
+    '１２月',
   ];
+  
+  const daysOfWeek = [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日',
+    ];
 
   // Set to true to use a 12 hour date format
   var format_12hour = false;
@@ -21,6 +31,7 @@ function displayClock() {
   var d = new Date();
   var mm = monthNames[d.getMonth()];
   var dd = d.getDate();
+  var dow = daysOfWeek[d.getDay()];
   var min = (mins = ('0' + d.getMinutes()).slice(-2));
   var hh = d.getHours();
   var ampm = '';
@@ -36,7 +47,8 @@ function displayClock() {
   document.getElementById('minutes').innerText = min + ampm;
 
   document.getElementById('month').innerText = mm;
-  document.getElementById('day').innerText = dd;
+  document.getElementById('day').innerText = dd + "日";
+  document.getElementById('dayOfWeek').innerText = dow;
 
   setTimeout(displayClock, 1000);
 }
